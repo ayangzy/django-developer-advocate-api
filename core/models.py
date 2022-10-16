053e9e9e90c18bcd9b3bea9de0425c9ff495d4a7
Ayange Felix <ayangefelix8@gmail.com>
2022-10-16 19:59:18 +0100

    add models, and configure static files

---
+++ core/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+class Company(models.Model):
+    advocates = models.ManyToManyField('Advocate', blank=True, related_name="companys")
+    name = models.CharField(max_length=50, blank=True)
+    logo = models.ImageField(blank=True, null=True)
+    summary = models.TextField(blank=True, null=True)
+    href = models.URLField(blank=True, null=True)
+   
+    def __str__(self):
+        return self.name
+    
+
+class Advocate(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.SET_NULL, null=True, blank=True)
+    name = models.CharField(max_length=50)
+    profile_pic = models.ImageField(blank=True, null=True)
+    short_bio = models.TextField(max_length=255, blank=True, null=True)
+    long_bio = models.TextField(blank=True, null=True)
+    advocate_years_exp = models.PositiveIntegerField(default=1)
+    links = models.JSONField(blank=True)
+    
+    def __str__(self):
+        return self.name
+    
--- devAdvocate/settings.py
+++ devAdvocate/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "core"
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = '/images/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
