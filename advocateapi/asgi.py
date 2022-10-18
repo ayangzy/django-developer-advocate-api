76c0943e0b9398c0b52741812f39a89e55e17d09
Ayange Felix <ayangefelix8@gmail.com>
2022-10-18 13:59:27 +0100

    rename project

--- advocateapi/asgi.py
+++ advocateapi/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "devAdvocate.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "advocateapi.settings")
 
 application = get_asgi_application()
--- advocateapi/wsgi.py
+++ advocateapi/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "devAdvocate.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "advocateapi.settings")
 
 application = get_wsgi_application()
